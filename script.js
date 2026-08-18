ecc4b18c8149897b7fb691bd382f68fdca1a6bb9
hoorash4 <hoorash4@gmail.com>
2026-08-18 19:46:55 +0900

    Update script.js

--- script.js
+++ script.js
@@ -391,7 +391,7 @@ async function confirmDeleteTarget() {
 // 헬퍼 함수
 function getConditionText(condition) {
   switch (condition) {
-    case 'changed': return '단순 값 변경';
+    case 'changed': return '값 변동 감지';
     case 'gte': return '목표값 상향 돌파';
     case 'lte': return '목표값 하향 돌파';
     case 'cross': return '목표값 상/하향 돌파';
