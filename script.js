a5ef4e0144564230904d0aface3b91f264dfd3ef
hoorash4 <hoorash4@gmail.com>
2026-08-20 22:27:35 +0900

    Connect grip drag drop events

--- script.js
+++ script.js
@@ -291,7 +291,7 @@ function renderTargetItem(item, globalIndex, isFirstVisible, isLastVisible) {
            ondragend="handleDragEnd(event)">
         <div class="flex items-center gap-3 min-w-0 flex-1">
           <i class="touch-drag-handle fa-solid fa-grip-vertical text-slate-600 hover:text-slate-400 cursor-grab active:cursor-grabbing px-1"
-             ondragstart="handleDragStart(event, ${globalIndex})"\n             ondragend="handleDragEnd(event)"\n             onpointerdown="handleTouchDragStart(event, ${globalIndex})"
+             ondragstart="handleDragStart(event, ${globalIndex})"\n             ondragend="handleDragEnd(event)"\n             ondragstart="handleDragStart(event, ${globalIndex})"\n             ondragend="handleDragEnd(event)"\n             onpointerdown="handleTouchDragStart(event, ${globalIndex})"
              onpointermove="handleTouchDragMove(event)"
              onpointerup="handleTouchDragEnd(event)"
              onpointercancel="handleTouchDragCancel(event)"></i>
