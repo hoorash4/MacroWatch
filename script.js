54400f150e5e46c026f505553496bcdfc0220984
hoorash4 <hoorash4@gmail.com>
2026-08-20 20:56:36 +0900

    Compact add tab label below 670px

--- script.js
+++ script.js
@@ -212,7 +212,7 @@ function renderTrackTabs() {
   addButton.type = 'button';
   addButton.className = 'sheet-tab sheet-tab-add';
   addButton.setAttribute('role', 'button');
-  addButton.innerHTML = '<span class="tab-label-wide">+ Tab</span><span class="tab-label-compact">+ Tab</span>';
+  addButton.innerHTML = '<span class="tab-label-wide">+ Tab</span><span class="tab-label-compact">+Tab</span>';
   addButton.addEventListener('click', showAddTrackNotice);
   tabsEl.appendChild(addButton);
 }
